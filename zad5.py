1a269715ce535f672431c5cd1cd7c1af746f6d12
suaviq <alasen24@gmail.com>
2025-03-16 16:49:23 +0100

    fix: input

--- zad5.py
+++ zad5.py
@@ -4,15 +4,23 @@
 # b. Sprawdź, jak działa przesunięcie dla różnych wartości.
 import numpy as np
 import cv2
-import imutils 
+import imutils
 
 image = cv2.imread("image.jpg")
 # cv2.imshow("Original", image)
 width = image.shape[1]
 height = image.shape[0]
 print(f"Wielkosc obrazka: {width}, {height}")
-x = input('Wybierz przesuniecie na osi X (prawo - dodatnie, lewo - ujemne wartosci): ')
-y = input('Wybierz przesuniecie na osi Y (dol - dodatnie, gora - ujemne wartosci): ')
+
+x = 0
+y = 0
+
+try:
+    x = int(input('Wybierz przesuniecie na osi X (prawo - dodatnie, lewo - ujemne wartosci): '))
+    y = int(input('Wybierz przesuniecie na osi Y (dol - dodatnie, gora - ujemne wartosci): '))
+except ValueError:
+    print('Napisz liczbe!!!!( ｡ •̀ ⤙ •́ ｡ )')
+    exit(1)
 
 shifted = imutils.translate(image, x, y)
 cv2.imshow("Shifted Down and Right", shifted)
